0e0b85caa3ae1760187655870a858516579d7783
Dgarzonac <123409139+Dgarzonac@users.noreply.github.com>
2023-08-27 19:01:44 -0500

    encapsulacion

--- televisores/marca.py
+++ televisores/marca.py
@@ -1,11 +1,17 @@
 class Marca:
+    _nombre = None
+
     def __init__(self, nombre):
-        self.nombre=nombre
+        self.nombre = nombre
+
     def setMarca(self,nombre):
-        self.nombre=nombre
+        self.nombre = nombre
+
     def getMarca(self):
         return self.nombre
+    
     def setNombre(self,nombre):
-        self.nombre=nombre
+        self.nombre = nombre
+        
     def getNombre(self):
         return self.nombre
--- televisores/tv.py
+++ televisores/tv.py
@@ -1,11 +1,11 @@
 class TV:
     _numTV=0
     def __init__(self, marca, estado):
-        self._marca = marca
-        self._estado = estado
-        self._canal = 1
-        self._precio = 500
-        self._volumen = 1
+        self.marca = marca
+        self.estado = estado
+        self.canal = 1
+        self.precio = 500
+        self.volumen = 1
         self._numTV = 0
         self._control = None
         TV._numTV+=1
